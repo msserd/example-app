4e46b042fac83af499504578c2024e248e7a5777
msserd <msserd@mail.ru>
2026-01-26 12:53:15 +0300

    Added day_11 task

---
+++ database/migrations/2026_01_26_093530_create_projects_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('projects')){
+            Schema::create('projects', function (Blueprint $table) {
+                $table->id();
+                $table->string('title', 255);
+                $table->foreignId('owner_id')->constrained('users');
+                $table->boolean('is_active')->default(true);
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
